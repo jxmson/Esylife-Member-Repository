689a11648c9a2233557cee05b0bb6a4b4b6ae88a
Nthabeleng jimson <56711591+jxmson@users.noreply.github.com>
2022-07-25 09:59:48 +0200

    update

---
+++ EsyLife/frmTransaction.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Esylife_POS
+{
+    partial class frmTransaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTranNo = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDiscountCode = new System.Windows.Forms.TextBox();
+            this.btnApplyDiscount = new System.Windows.Forms.Button();
+            this.btnFinishTransaction = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrTime = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Discount: ";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(15, 176);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(77, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Discount Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Transaction no.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(15, 136);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(43, 13);
+            this.lblDate.TabIndex = 4;
+            this.lblDate.Text = "000000";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total: ";
+            // 
+            // lblTranNo
+            // 
+            this.lblTranNo.AutoSize = true;
+            this.lblTranNo.Location = new System.Drawing.Point(15, 75);
+            this.lblTranNo.Name = "lblTranNo";
+            this.lblTranNo.Size = new System.Drawing.Size(49, 13);
+            this.lblTranNo.TabIndex = 6;
+            this.lblTranNo.Text = "0000000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(168, 399);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 399);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 428);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "label10";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblDiscount);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnFinishTransaction);
+            this.panel1.Controls.Add(this.btnApplyDiscount);
+            this.panel1.Controls.Add(this.txtDiscountCode);
+            this.panel1.Controls.Add(this.lblTranNo);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblDate);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label);
+            this.panel1.Location = new System.Drawing.Point(600, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtDiscountCode
+            // 
+            this.txtDiscountCode.Location = new System.Drawing.Point(18, 192);
+            this.txtDiscountCode.Name = "txtDiscountCode";
+            this.txtDiscountCode.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscountCode.TabIndex = 10;
+            // 
+            // btnApplyDiscount
+            // 
+            this.btnApplyDiscount.Location = new System.Drawing.Point(18, 227);
+            this.btnApplyDiscount.Name = "btnApplyDiscount";
+            this.btnApplyDiscount.Size = new System.Drawing.Size(75, 23);
+            this.btnApplyDiscount.TabIndex = 10;
+            this.btnApplyDiscount.Text = "Apply";
+            this.btnApplyDiscount.UseVisualStyleBackColor = true;
+            // 
+            // btnFinishTransaction
+            // 
+            this.btnFinishTransaction.Location = new System.Drawing.Point(18, 348);
+            this.btnFinishTransaction.Name = "btnFinishTransaction";
+            this.btnFinishTransaction.Size = new System.Drawing.Size(75, 23);
+            this.btnFinishTransaction.TabIndex = 11;
+            this.btnFinishTransaction.Text = "Finish";
+            this.btnFinishTransaction.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(99, 348);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(57, 399);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 13;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0.00";
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.Location = new System.Drawing.Point(123, 289);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(28, 13);
+            this.lblDiscount.TabIndex = 11;
+            this.lblDiscount.Text = "0.00";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(140, 8);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(34, 13);
+            this.lblTime.TabIndex = 14;
+            this.lblTime.Text = "00:00";
+            // 
+            // tmrTime
+            // 
+            this.tmrTime.Tick += new System.EventHandler(this.tmrTime_Tick);
+            // 
+            // frmTransaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Name = "frmTransaction";
+            this.Text = "frmTransaction";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTranNo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtDiscountCode;
+        private System.Windows.Forms.Button btnApplyDiscount;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFinishTransaction;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer tmrTime;
+    }
+}
---
+++ EsyLife/frmTransaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Esylife_POS
+{
+    public partial class frmTransaction : Form
+    {
+        public frmTransaction()
+        {
+            InitializeComponent();
+            tmrTime.Start();
+        }
+
+        private void tmrTime_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("hh:mm tt");
+        }
+        public string GetTransactionNumber()
+        {
+            string date = DateTime.Now.ToString("yyyyMMdd");
+            int increment = 1001;
+            string transnum = date + increment.ToString();
+            increment++;              
+
+            return transnum;
+        }
+    }
+}
